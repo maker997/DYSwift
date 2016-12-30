967f26240a7f901e4b567f275872bdc5ede549be
maker997 <806160921@qq.com>
2016-12-30 10:06:52 +0800

    更换 model 类

--- DYSwift/Classes/Room/model/Live.swift
+++ DYSwift/Classes/Room/model/Live.swift
@@ -1,103 +1,44 @@
 //
-//	Live.swift
+//  Live1.swift
+//  DYSwift
 //
-//	Create by taonei on 28/12/2016
-//	Copyright © 2016. All rights reserved.
-//	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+//  Created by 刘甲奇 on 2016/12/29.
+//  Copyright © 2016年 maker. All rights reserved.
+//
 
-import Foundation
 import UIKit
-class Live: NSObject{
 
-	var city : String!
-	var creator : Creator!
-	var group : Int!
-	var id : String!
-	var link : Int!
-	var multi : Int!
-	var name : String!
-	var onlineUsers : Int!
-	var optimal : Int!
-	var rotate : Int!
-	var shareAddr : String!
-	var slot : Int!
-	var streamAddr : String!
-	var version : Int!
+class Live: NSObject {
+    var city : String!
+    var creator :[String:Any]?{
+        didSet{
+            guard let creators = creator else {return}
+            creatorModel = Creator(fromDictionary: creators)
+        }
+    }
+    var name : String!
+    var online_users : Int!
+    var stream_addr : String!
+    var creatorModel: Creator!
+    
     var rowHeight: CGFloat = 0.0
     
+    init(fromDictionary dictionary: [String:Any]) {
+        super.init()
+        setValuesForKeys(dictionary)
+    }
     
-
-	/**
-	 * Instantiate the instance using the passed dictionary values to set the properties values
-	 */
-	init(fromDictionary dictionary: NSDictionary){
-		city = dictionary["city"] as? String
-		if let creatorData = dictionary["creator"] as? NSDictionary{
-				creator = Creator(fromDictionary: creatorData)
-			}
-		group = dictionary["group"] as? Int
-		id = dictionary["id"] as? String
-		link = dictionary["link"] as? Int
-		multi = dictionary["multi"] as? Int
-		name = dictionary["name"] as? String
-		onlineUsers = dictionary["online_users"] as? Int
-		optimal = dictionary["optimal"] as? Int
-		rotate = dictionary["rotate"] as? Int
-		shareAddr = dictionary["share_addr"] as? String
-		slot = dictionary["slot"] as? Int
-		streamAddr = dictionary["stream_addr"] as? String
-		version = dictionary["version"] as? Int
-	}
-
-	/**
-	 * Returns all the available property values in the form of NSDictionary object where the key is the approperiate json key and the value is the value of the corresponding property
-	 */
-	func toDictionary() -> NSDictionary
-	{
-		let dictionary = NSMutableDictionary()
-		if city != nil{
-			dictionary["city"] = city
-		}
-		if creator != nil{
-			dictionary["creator"] = creator.toDictionary()
-		}
-		if group != nil{
-			dictionary["group"] = group
-		}
-		if id != nil{
-			dictionary["id"] = id
-		}
-		if link != nil{
-			dictionary["link"] = link
-		}
-		if multi != nil{
-			dictionary["multi"] = multi
-		}
-		if name != nil{
-			dictionary["name"] = name
-		}
-		if onlineUsers != nil{
-			dictionary["online_users"] = onlineUsers
-		}
-		if optimal != nil{
-			dictionary["optimal"] = optimal
-		}
-		if rotate != nil{
-			dictionary["rotate"] = rotate
-		}
-		if shareAddr != nil{
-			dictionary["share_addr"] = shareAddr
-		}
-		if slot != nil{
-			dictionary["slot"] = slot
-		}
-		if streamAddr != nil{
-			dictionary["stream_addr"] = streamAddr
-		}
-		if version != nil{
-			dictionary["version"] = version
-		}
-		return dictionary
-	}
-
+    override func setValue(_ value: Any?, forUndefinedKey key: String) {}
 }
+
+
+
+
+
+
+
+
+
+
+
+
